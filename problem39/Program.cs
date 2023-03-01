a8eb77478b9b63fc0ec540418b849702ef23af7b
ezz2003 <ezz2003@yandex.ru>
2023-03-01 14:21:31 +1000

    solved the problem39

---
+++ problem39/Program.cs
@@ -0,0 +1,23 @@
+﻿// 39. напишите программу, которая перевернёт одномерный массив (последний элемент будет на первом месте, первый - на последнем, и т.д.)
+// 
+// [1, 2, 3, 4, 5] -> [5, 4, 3, 2, 1]
+
+
+string MakeStrPrintArr(int[] arr)
+{
+  string strPrn = string.Empty;
+  for (int i = 0; i < arr.Length - 1; i++) strPrn += $"{arr[i]}, ";
+  strPrn += arr.Last();
+  return strPrn;
+}
+
+void SwapArray(int[] arr)
+{
+  for (int i = 0; i < arr.Length / 2; i++) (arr[i], arr[arr.Length - i - 1]) = (arr[arr.Length - i - 1], arr[i]);
+}
+
+
+int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+Console.Write($"[{MakeStrPrintArr(array)}] -> ");
+SwapArray(array);
+Console.WriteLine($"[{MakeStrPrintArr(array)}]");
