37ab938052cc352e5c42d7704dcfe03a5b074bdb
ezz2003 <ezz2003@yandex.ru>
2023-03-03 14:08:42 +1000

    solved the problem41

---
+++ problem41/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 
+// 0, 7, 8, -2, -2 -> 2
+// 
+// 1, -7, 567, 89, 223-> 3
+
+int InputInt(string message)
+{
+  Console.Write(message + ": ");
+  return Convert.ToInt32(Console.ReadLine());
+}
+
+string MakeStringToPrintArray(int[] arr)
+{
+  string strPrn = string.Empty;
+  for (int i = 0; i < arr.Length - 1; i++) strPrn += $"{arr[i]}, ";
+  strPrn += arr.Last();
+  return $"{strPrn}";
+}
+
+
+int[] InputAnyNumbers(int quantity)
+{
+  int[] usersArray = new int[quantity];
+  for (int i = 0; i < quantity; i++)
+  {
+    usersArray[i] = InputInt($"Введите {i + 1} число");
+  }
+  return usersArray;
+}
+
+int HowManyGreaterZero(int[] arr)
+{
+  int count = 0;
+  for (int i = 0; i < arr.Length; i++)
+  {
+    if (arr[i] > 0) { count++; }
+  }
+  return count;
+}
+
+int quantityM = InputInt("Сколько чисел будете вводить?");
+int[] usersNumbers = InputAnyNumbers(quantityM);
+Console.WriteLine($"{MakeStringToPrintArray(usersNumbers)} -> {HowManyGreaterZero(usersNumbers)}");
